e49ec51d73bfdf6b1a77f89e8d263b45525105fb
Idmar Ramos <idmarjr@gmail.com>
2011-09-01 03:32:51 -0300

    First release of Sweet Feedback. 	- HTML5 marckup; 	- CSS3 proprieties; 		- Multiple backgrounds; 		- Animations (-moz and -webkit);

---
+++ sweetfeedback.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+	
+	//Background noise using Noisy.
+	$('body').noisy({
+		'intensity' : 1,
+		'size' : 100,
+		'opacity' : 0.08,
+		'fallback' : '',
+		'monochrome' : false
+	}).css('background-color', '#d6d6d6');
+
+});
